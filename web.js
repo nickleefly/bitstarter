27e61652e017528c90d8c9c4cef091154527f60a
nickleefly <nickleefly@gmail.com>
2013-07-10 20:52:13 +0800

    add readFileSync and buffer toString method

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
+var buffer = fs.readFileSync('index.html');
+var out = buffer.toString();
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(out);
 });
 
 var port = process.env.PORT || 5000;
